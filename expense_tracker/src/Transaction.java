d8c8a9212a7f980c48f264556845f102cec3c913
Srinanda Yallapragada <srinanda.yallapragada@gmail.com>
2025-03-04 23:05:36 -0500

    updated comments in Transaction.java

--- expense_tracker/src/Transaction.java
+++ expense_tracker/src/Transaction.java
@@ -1,4 +1,7 @@
 
+/**
+* Models a financial transaction from ExpenseTracker.
+*/
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,34 +13,66 @@ public class Transaction {
   private String category;
   private String timestamp;
 
+  /**
+   * Creates new transaction with the amount and category values passed in.
+   *
+   * @param amount   The transaction amount.
+   * @param category The category of the transaction.
+   */
   public Transaction(double amount, String category) {
     this.amount = amount;
     this.category = category;
     this.timestamp = generateTimestamp();
   }
 
+  /**
+   * Returns the amount of the transaction.
+   *
+   * @return The transaction amount.
+   */
   public double getAmount() {
     return amount;
   }
 
+  /**
+   * Sets the amount of the transaction.
+   *
+   * @return void.
+   */
   public void setAmount(double amount) {
     this.amount = amount;
   }
 
+  /**
+   * Sets timestamp for trasaction 
+   * @return void
+   **/
+  public void setTimestamp(String timestamp) {
+    this.timestamp = timestamp;
+  }
+  /**
+  * Gets category of transaction
+  * @return String 
+  */
   public String getCategory() {
     return category;
   }
 
+  /**
+   * Sets the category of the transaction.
+   *
+   * @return void.
+   */
   public void setCategory(String category) {
-    this.category = category; 
+    this.category = category;
   }
-  
+
   public String getTimestamp() {
     return timestamp;
   }
 
   private String generateTimestamp() {
-    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");  
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
     return sdf.format(new Date());
   }
 
